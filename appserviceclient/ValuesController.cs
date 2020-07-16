fa0862dcd44ae570594e21f4bbab4f328cd5eadb
Mohammed Fasiulla <mofasiul@microsoft.com>
2020-07-16 12:07:40 -0700

    first commit

---
+++ appserviceclient/ValuesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Http;
+
+namespace appserviceclient
+{
+    public class ValuesController : ApiController
+    {
+        public string certHeader = "";
+        public string errorString = "";
+        private X509Certificate2 certificate = null;
+        public string certThumbprint = "";
+        public string certSubject = "";
+        public string certIssuer = "";
+        public string certSignatureAlg = "";
+        public string certIssueDate = "";
+        public string certExpiryDate = "";
+        public bool isValidCert = false;
+
+
+        [HttpHead]       
+        public IHttpActionResult Head()
+        {
+            if (validatecert(base.Request.Headers))
+                return Ok();
+            else
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Certificate Invalid"));
+        }
+
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            if (validatecert(base.Request.Headers))
+                return new string[] { "value1", "value2" };
+
+            else
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Certificate Invalid"));
+
+        }
+
+        private bool validatecert(HttpRequestHeaders headers)
+        {
+            headers.TryGetValues("X-ARR-ClientCert", out IEnumerable<string> headerValues);
+            var certHeader = headerValues.FirstOrDefault();           
+            if (!String.IsNullOrEmpty(certHeader))
+            {
+                try
+                {
+                    byte[] clientCertBytes = Convert.FromBase64String(certHeader);
+                    certificate = new X509Certificate2(clientCertBytes);
+                    certSubject = certificate.Subject;
+                    certIssuer = certificate.Issuer;
+                    certThumbprint = certificate.Thumbprint;
+                    certSignatureAlg = certificate.SignatureAlgorithm.FriendlyName;
+                    certIssueDate = certificate.NotBefore.ToShortDateString() + " " + certificate.NotBefore.ToShortTimeString();
+                    certExpiryDate = certificate.NotAfter.ToShortDateString() + " " + certificate.NotAfter.ToShortTimeString();
+                }
+                catch (Exception ex)
+                {
+                    errorString = ex.ToString();
+                }
+                finally
+                {
+                    isValidCert = IsValidClientCertificate();
+                    
+                }
+            }
+            else
+            {
+                return false;
+            }
+
+            return isValidCert;
+        
+        }
+
+        private bool IsValidClientCertificate()
+        {
+           //DO your custom cert validation here 
+
+            if (certificate.Thumbprint.Trim().ToLower() == "28134dc467335ba86a4f8bda86504c8b62068903") return true;
+
+            return false;
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public string Post([FromBody] string value)
+        {
+            if (validatecert(base.Request.Headers))
+                return "success";
+
+            else
+                throw new HttpResponseException(Request.CreateErrorResponse(HttpStatusCode.Forbidden, "Certificate Invalid"));
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ clientcert/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.IO;
+using System.Net.Http;
+
+namespace clientcert
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var host = "https://webapp.ewebsites.net";
+            var endpoint = "/api/values";          
+            string CertificatePath = @"C:\Users\Documents\ClientCertificate.cer";          
+            string filetoUpload = @"c:\users\Documents\fieltopost.json"; //This is the file that will be passed as part of the actual POST.  Make sure its grater than 100 KB
+            string postData = File.ReadAllText(filetoUpload);
+            StringContent myStringContent = new StringContent(postData, Encoding.UTF8, "application/x-www-form-urlencoded");
+
+
+            //Scenario 1: Here the POST call is made directly by passing the client certificate. Since the POST Size is huge (~100KB), the POST call hangs forever. 
+            var _clientHandler = new HttpClientHandler();
+            _clientHandler.ClientCertificates.Add(new X509Certificate2(CertificatePath, ""));
+            HttpClient client = new HttpClient(_clientHandler);
+            client.BaseAddress = new Uri(host);
+            var httppostresponseTask =  client.PostAsync(host + endpoint, myStringContent);
+           
+
+
+            //Scenario 2: Here the POST call is made after making an HEAD call by passing the client certificate. 
+            var _clientHandlersuccess = new HttpClientHandler();
+            _clientHandlersuccess.ClientCertificates.Add(new X509Certificate2(CertificatePath, ""));
+            HttpClient clientSuccess = new HttpClient(_clientHandlersuccess);
+            clientSuccess.BaseAddress = new Uri(host);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Head, endpoint);
+            HttpResponseMessage response = await clientSuccess.SendAsync(request);//First Make a HEAD request
+            if (response.IsSuccessStatusCode)
+            {
+                //If the Head Request is sucessfull then make a POST call.
+                var httpResponseMessage = await clientSuccess.PostAsync(host + endpoint, myStringContent);
+                if (httpResponseMessage.IsSuccessStatusCode)
+                {
+                    Console.WriteLine("Scenario 2: The HTTP POST call succeeded with client cert authentication");
+                }
+            }
+
+            //This call never completes and hang forever. 
+            var httppostresponse = await httppostresponseTask;
+            if (httppostresponse.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Scenario 1: The HTTP POST call succeeded with client cert authentication");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
